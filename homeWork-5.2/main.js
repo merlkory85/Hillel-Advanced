0372c119a89779a472ec2f2a98b12e839bc1bfa6
Yavorskyi Serhii <merlkory1985@gmail.com>
2025-09-08 21:40:49 +0300

    make hw 5.2

--- homeWork-5.2/main.js
+++ homeWork-5.2/main.js
@@ -1,52 +1,10 @@
 'use strict'
 
-const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
-const dollar = 26;
+const dollarExchangeRate = 26;
 
-for (let num of numbers) {
+for (let i = 10; i <= 100; i = i + 10) {
 
-    switch (num) {
-        case numbers[0]:
-            console.log(`${numbers[0]} долярів коштує ${numbers[0] * dollar} гривнів) !!!`)
-            break;
+    let total = dollarExchangeRate * i;
 
-        case numbers[1]:
-            console.log(`${numbers[1]} долярів коштує ${numbers[1] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[2]:
-            console.log(`${numbers[2]} долярів коштує ${numbers[2] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[3]:
-            console.log(`${numbers[3]} долярів коштує ${numbers[3] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[4]:
-            console.log(`${numbers[4]} долярів коштує ${numbers[4] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[5]:
-            console.log(`${numbers[5]} долярів коштує ${numbers[5] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[6]:
-            console.log(`${numbers[6]} долярів коштує ${numbers[6] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[7]:
-            console.log(`${numbers[7]} долярів коштує ${numbers[7] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[8]:
-            console.log(`${numbers[8]} долярів коштує ${numbers[8] * dollar} гривнів) !!!`)
-            break;
-
-        case numbers[9]:
-            console.log(`${numbers[9]} долярів коштує ${numbers[9] * dollar} гривнів) !!!`)
-            break;
-
-        default:
-            break;
-    }
-}
+    console.log(`${i} долярів коштує ${total} гривнів) !!!`)
+} 
